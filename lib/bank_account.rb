057bbe3dbfade1a92e2888f2ba9eb3eab03dad2b
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-18 04:31:47 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
-class BankAccount
+ class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(quantity)
+    @balance += quantity
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+  
+  def close_account 
+    @status = "closed"
+    @balance = 0
+  end
 
 end
