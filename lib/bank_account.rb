1b38ee81774fe3047802b1c2122c7e564fdb6069
Rene Cocom <fasnimo@gmail.com>
2019-07-31 17:40:37 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class BankAccount
+  attr_accessor :bank_account, :balance, :status
+  attr_reader :name, :deposit
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+# binding.pry
+  def deposit(money)
+    balance = @balance += 1
+    money = @deposit
+    balance << money
+  end
 end
